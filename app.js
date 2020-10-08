a732483a5777fb98298c7893477a3f58cce275a6
Pratikshit26 <pratikshitpfd@gmail.com>
2020-10-08 16:53:48 +0530

    project scaffolding

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Setup ERROR Handlers
+const errorHandlers = require("./handlers/errorHandlers");
+app.use(errorHandlers.notFound);
+app.use(errorHandlers.mongoseErrors);
+if (process.env.ENV === "DEVELOPMENT") {
+  app.use(errorHandlers.developmentErrors);
+} else {
+  app.use(errorHandlers.productionErrors);
+}
+
+module.exports = app;
