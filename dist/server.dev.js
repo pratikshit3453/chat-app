8057740a657fe8f6ae880f9ae04e1d2b1986039a
Pratikshit26 <pratikshitpfd@gmail.com>
2020-10-09 10:11:18 +0530

    added backend

--- dist/server.dev.js
+++ dist/server.dev.js
@@ -1,28 +1,28 @@
+"use strict";
+
 require("dotenv").config();
 
-const mongoose = require("mongoose");
-mongoose.connect(
-  "mongodb+srv://pratik:pratik123@cluster0.4gmto.mongodb.net/Cluster0?retryWrites=true&w=majority",
-  {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  }
-);
+var mongoose = require("mongoose");
 
+mongoose.connect("mongodb+srv://pratik:pratik123@cluster0.4gmto.mongodb.net/Cluster0?retryWrites=true&w=majority", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
 mongoose.connection.once("open", function () {
   console.log("mongodb connection open");
 });
 mongoose.connection.on("error", function (err) {
   console.log(err.message);
-});
+}); //Bring in the models
 
-//Bring in the models
 require("./modals/user");
+
 require("./modals/Chatroom");
+
 require("./modals/Message");
 
-const app = require("./app");
+var app = require("./app");
 
-app.listen(8000, () => {
+app.listen(8000, function () {
   console.log("Server listening on port 8000");
 });
